e1b84e88262887ca08bbad2b6ac2b1cc2406632e
Ulrich Dultinger <udultinger@gmx.at>
2022-11-17 11:27:14 +0100

    feat: create exercise model, create datastore

---
+++ src/models/store.ts
@@ -0,0 +1,14 @@
+import { BehaviorSubject } from "rxjs";
+import {Exercise} from "./exercise"
+
+export interface Model {
+    readonly exercises: Exercise[]
+}
+
+const initialState: Model = {
+    exercises: []
+}
+
+const store = new BehaviorSubject<Model>(initialState);
+
+export default store;
