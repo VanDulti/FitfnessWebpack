34f323ac98b388e53b37cda73c3d238bcef30d7f
Ulrich Dultinger <udultinger@gmx.at>
2022-12-15 12:35:35 +0100

    feat: use navigo router refactor: rename classes

--- src/components/app-component.ts
+++ src/components/app-component.ts
@@ -1,34 +1,54 @@
-import {html, render} from "lit-html"
+import Navigo from 'navigo';
+import { html, render } from "lit-html"
 import "./exercise-table-component"
-import "./exercise-component"
+import "./exercise-detail-component"
+import { Console } from 'console';
 
 const appComponentTemplate = html`
-    <exercise-table-component id="table"></exercise-table-component>
-    <exercise-component></exercise-component>
+    <h1>Fitfness</h1>
+    <nav><a href="/" data-navigo>Home</a></nav>
+    <div id="content">
+        <exercise-table></exercise-table>
+        <exercise-detailed></exercise-detailed>
+    </div>
 `
+
+const router = new Navigo('/');
 
 class AppComponent extends HTMLElement {
     constructor() {
         super()
-        this.attachShadow({mode: "open"})
+        this.attachShadow({ mode: "open" })
     }
     connectedCallback() {
         console.log("connected")
         this.render()
-
+        router.on({
+            '/': () => {
+                console.log("/")
+                this.renderContent("exercise-table")
+            },
+            '/exercises/:id': ({ data }: any) => {
+                console.log(data.id); // { id: 'xxx' }
+                this.renderContent("exercise-detailed")
+            }
+        }).notFound(function () {
+            console.log("/not found");
+        }).resolve();
+    }
+    renderContent(selector: string) {
+        const content = this.shadowRoot.querySelector("#content")
+        for (const e of content.children) {
+            (e as HTMLElement).style.display = "none"
+        }
+        const selected: HTMLElement = content.querySelector(selector)
+        selected.style.display = "block"
     }
     render() {
         render(appComponentTemplate, this.shadowRoot)
-        const exerciseTable = this.shadowRoot.getElementById("table")
-        const exerciseComponent: HTMLElement = this.shadowRoot.querySelector("exercise-component")
-        exerciseTable.addEventListener("exercise-selected", (e: CustomEvent) => {
-            const exercise = e.detail?.exercise
-            console.log("exercise-selected", exercise)
-            exerciseComponent.setAttribute("id", exercise.id)
-            exerciseTable.style.display = "none"
-            exerciseComponent.style.display = "block"
-        })
     }
 }
- 
+
+export default {router}
+
 customElements.define("app-component", AppComponent)
