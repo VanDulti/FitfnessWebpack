d772f44ec688b3dc7913b1f048177df942db382d
Lisa Heinz <20180069@students.htl-perg.ac.at>
2023-01-02 21:49:32 +0100

    Closes #18

--- src/components/exercise-table-component.ts
+++ src/components/exercise-table-component.ts
@@ -7,16 +7,14 @@ import Navigo from "navigo"
 import router from "./app-component"
 
 const tableTemplate = html`
-    <head>    
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
-    </head>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
+
     <style>
     .table tr:hover {
         background-color: #D8D8D8;
     }
     </style>
+
     <h1 class="title" style="font-family: Inter, sans-serif;">Übungsübersicht</h1>
     <div class="card"> 
         <div class="card-content">
