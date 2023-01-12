49dad2f1e0c004f136387f2718916b34df87ab70
Lisa Heinz <20180069@students.htl-perg.ac.at>
2023-01-12 07:52:16 +0100

    fix: css files

--- src/components/exercise-detail-component.ts
+++ src/components/exercise-detail-component.ts
@@ -4,7 +4,6 @@ import { Exercise } from "../model/exercise"
 import store from "../model/store"
 
 const template = html`
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
     <link rel="stylesheet" href="/src/css/exercise-detail.css"/>
 
     <div id="card" class="card"> 
