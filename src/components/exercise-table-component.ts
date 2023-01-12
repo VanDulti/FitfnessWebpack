de1f385be7ceceb5b30bf3383edb439e5976cbf2
Sebastian <sebastian@fuerlinger.org>
2023-01-12 08:51:03 +0100

    fear: searchfilter

--- src/components/exercise-table-component.ts
+++ src/components/exercise-table-component.ts
@@ -16,6 +16,17 @@ const tableTemplate = html`
 
     <div style="font-family: Inter, sans-serif">
         <h1 class="title">Übungsübersicht</h1>
+        <div style="font-family: Inter, sans-serif">
+        <div class="field has-addons">
+            <div class="control">
+                <input class="input" type="text" placeholder="Search by name or category" id="search-input">
+            </div>
+            <div class="control">
+                <button class="button is-info" id="search-button">
+                    Search
+                </button>
+            </div>
+        </div>
         <div class="card"> 
             <div class="card-content">
                 <table class="table" style="width: 1400px;margin: 0 auto;">    
@@ -50,14 +61,26 @@ const rowTemplate = (exercise: Exercise) => html
 
 class ExerciseTableComponent extends HTMLElement {
     private root: ShadowRoot
+    private exerciseList: Exercise[] = []
+
     constructor() {
         super()
         this.root = this.attachShadow({ mode: "closed" })
     }
     async connectedCallback() {
-        console.log("ExerciseTableComponent connected")
         exerciseService.fetch()
-        store.subscribe(model => this.render(model.exercises))
+        store.subscribe(model =>{
+            this.exerciseList = model.exercises
+            this.render(model.exercises)
+        })
+
+        const searchButton = this.root.querySelector("#search-button")
+        searchButton.addEventListener("click", ()=>{
+            const searchInput = this.root.querySelector("#search-input") as HTMLInputElement
+            const searchTerm = searchInput.value.toLocaleLowerCase()
+            const filteredExercises = this.exerciseList.filter(exercise => exercise.name.toLowerCase().includes(searchTerm))
+            this.render(filteredExercises)
+        })
     }
     private render(exercises: Exercise[]) {
         render(tableTemplate, this.root)
