515aaa177543fc5d0288775257abb7a0c2a04e4f
Lisa Heinz <20180069@students.htl-perg.ac.at>
2023-01-12 09:36:04 +0100

    fix: font works in chrome, reformat documents

--- src/exercise-service.ts
+++ src/exercise-service.ts
@@ -4,25 +4,25 @@ import produce from "immer"
 const url = "http://localhost:3000/exercises"
 
 class ExerciseService {
-    async fetch() {
-        const response = await fetch(url)
-        console.log(response)
-        const model = store.getValue()
-        const exercises: [Exercise] = await response.json()
-        const nextState = produce(model, draft => { draft.exercises = exercises })
-        store.next(nextState)
-    }
+  async fetch() {
+    const response = await fetch(url)
+    console.log(response)
+    const model = store.getValue()
+    const exercises: [Exercise] = await response.json()
+    const nextState = produce(model, draft => { draft.exercises = exercises })
+    store.next(nextState)
+  }
 
-    async postData(newExercise:Exercise) {
-        const response = await fetch(url, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify(newExercise)
-        })
-        return response.json()
-    }
+  async postData(newExercise: Exercise) {
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newExercise)
+    })
+    return response.json()
+  }
 }
 
 const exerciseService = new ExerciseService()
